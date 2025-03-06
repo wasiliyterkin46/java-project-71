78cc13b4e0d410711878c67a4bd1ef5d5864c116
wasiliyterkin46 <wasiliy_terkin@mail.ru>
2025-03-06 18:46:18 +0300

    Update hexlet.code.App.java: add option and parameters cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,8 +5,18 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command(name = "App", version = "App 1.0", mixinStandardHelpOptions = true)
+@Command(name = "gendiff", version = "gendiff 1.0", mixinStandardHelpOptions = true)
 public class App implements Runnable {
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    String format;
+
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    String filepath1;
+
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    String filepath2;
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
